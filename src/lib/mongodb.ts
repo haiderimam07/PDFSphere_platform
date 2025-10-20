aecb6f7779bda7304fd4a8960c23444aa73ae949
haiderimam07 <haiderimam1207@gmail.com>
2025-10-20 20:06:39 +0530

    db connection

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,20 +1,37 @@
 import mongoose from 'mongoose'
 
-const MONGODB_URI = process.env.MONGO;
+const MONGODB_URI = process.env.MONGO!;
 
 if (!MONGODB_URI) {
     throw new Error (" please define mongo environment variable")
 }
 
+let cached=global.mongoose
+if(!cached){
+    cached=global.mongoose={conn:null, promise:null}
+}
+
+
 async function connectToDatabase() {
-    if (mongoose.connection.readyState === 1) {
-        return mongoose;
+    if (cached.conn) {
+        return cached.conn;
     }
-    const opts = {
-        bufferCommands: false,
+    if(!cached.promise){
+        const opts={
+            bufferCommands:true,
+            maxPoolSize:10,
+        };
+        mongoose.connect(MONGODB_URI,opts).then(()=>mongoose.connection)
     }
-    await mongoose.connect(MONGODB_URI!, opts);
-    return mongoose;
+
+    try {
+        cached.conn=await cached.promise;
+    } catch (error) {
+        cached.promise=null
+        throw new Error("conn DB file")
+    }
+
+    return cached.conn;
 }
 
 export default connectToDatabase;
